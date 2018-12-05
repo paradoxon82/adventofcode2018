8da9ae311e29bf82f9fc38528b0b50814b6e8c01
Daniel Fink <daniel.ja.fink@gmail.com>
2018-12-05 19:15:55 +0100

    first try at faster version, that just counts resulting lenght without creating intermediate strings

---
+++ day5/day5_fast.rb
@@ -0,0 +1,130 @@
+#!/usr/bin/ruby
+
+#require 'rspec/autorun'
+
+class Polymer
+
+  def initialize(initial_sequence)
+    @initial_sequence = initial_sequence.chars
+  end
+
+  # reacts if the two chars are the same but different case
+  def reacts?(left, right)
+    if left != right 
+      # case insensitive compare
+      left.casecmp(right) == 0
+    else
+      false
+    end
+  end
+
+  # go back along the way of the sequence to the left and right of the starting point, to find out how the length will retract
+  def retraction(sequence, starting_point)
+    ret = 0
+    while reacts?(sequence[starting_point-ret-1], sequence[starting_point+ret+2])
+      ret += 1
+    end
+    ret * 2
+  end
+
+  def length_after_reaction
+    length_after_reaction_of(@initial_sequence)
+  end
+
+  def length_after_reaction_of(sequence)
+    remainder = []
+    # at least one reaction was done
+    found_one = false
+    retrun false if sequence.empty?
+
+    count = 0
+    i = 0
+    end_index = sequence.size - 1
+    while i < end_index
+      if reacts?(sequence[i], sequence[i+1])
+        ret = retraction(sequence, i)
+        count -= ret
+        i += ret
+      else
+        count += 1
+      end
+      i += 1
+    end
+    count
+  end
+
+end
+
+class Reactor
+  attr_reader :sequence_string, :uniqe_units
+
+  def initialize()
+    @sequence_string = ''
+  end
+
+  def add_line(line)
+    @sequence_string.concat(line)
+  end
+
+  def length_after_reaction
+    Polymer.new(@sequence_string).length_after_reaction
+  end
+
+  # def shortest_length_after_reaction
+  #   @uniqe_units = @sequence_string.upcase.chars.uniq
+  #   @result_sizes = {}
+  #   @uniqe_units.each do |unit|
+  #     puts "unit: #{unit}"
+  #     fixed_sequence = @sequence_string.delete([unit, unit.downcase].join)
+  #     puts "fixed_sequence size: #{fixed_sequence.size}"
+  #     result = Polymer.new(fixed_sequence).result
+  #     @result_sizes[unit] = result.size
+  #     puts "result size: #{result.size}"
+  #   end
+
+  #   unit, size = @result_sizes.min_by {|unit, size| size }
+  #   puts "shortest without unit #{unit}"
+  #   size
+  # end
+
+end
+
+# RSpec.describe Polymer do
+#   let(:input) {
+#     'dabAcCaCBAcCcaDA'
+#   }
+
+#   describe '#square_overlap_count' do
+#     it 'calculates the example' do
+#       expect(Claims.new(input).square_overlap_count).to eq(4)
+#     end
+#   end
+
+#   describe '#non_overlapping_claims' do
+#     it 'returns the non-overlapping squares' do
+#       expect(Claims.new(input).non_overlapping_claims.map(&:id)).to eq(['3'])
+#     end
+#   end
+# end
+
+reactor = Reactor.new
+File.open(ARGV[0]).each_line do |line|
+  reactor.add_line(line.strip)
+end
+puts "result size: #{reactor.length_after_reaction}"
+# puts "shortest result: #{reactor.shortest_length_after_reaction}"
+
+# reactor = Reactor.new
+# reactor.add_line('dabAcCaCBAcCcaDA')
+# puts "input: #{reactor.sequence_string}, result: #{reactor.result_string}" 
+# puts "matches: #{reactor.result_string == 'dabCBAcaDA'}"
+
+# RSpec.describe Reactor do
+#   describe '#result' do
+#     it 'returns the resulting polymer' do
+#       result = 'dabCBAcaDA'
+
+#       expect(Polymer.new('dabAcCaCBAcCcaDA').result == result)
+#     end
+#   end
+# end
