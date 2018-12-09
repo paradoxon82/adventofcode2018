9489c8a02a32a8a55854444794f7d50946445257
Daniel Fink <daniel.ja.fink@gmail.com>
2018-12-09 16:30:17 +0100

    also finished part 2

--- day8/day8_1.rb
+++ day8/day8_1.rb
@@ -13,6 +13,7 @@ class TreeNode
     @child_count = 0
     @metadata_count = 0
     @name = TreeNode.next_name()
+    @verbose = false
   end
 
   def self.next_name
@@ -26,7 +27,7 @@ class TreeNode
   end
 
   def consume(data)
-    puts "node: #{self.name}, consume #{data}, state #{@state}"
+    puts "node: #{self.name}, consume #{data}, state #{@state}" if @verbose
     case @state
     when :child_count
       @child_count = data
@@ -38,7 +39,6 @@ class TreeNode
       else
         @state = :follow_children
       end
-      puts "state after metadata_count #{@state}, #{@child_count}, #{@metadata_count}"
     when :follow_children
       @state = :descent
     when :descent
@@ -51,15 +51,15 @@ class TreeNode
 
     ret = if @state == :full
       if @parent.nil?
-        puts "returning from root #{self.name} and done!"
+        puts "returning from root #{self.name} and done!" if @verbose
       else
-        puts "returning from #{self.name} to #{@parent.name}"
+        puts "returning from #{self.name} to #{@parent.name}" if @verbose
       end
       
       @parent
     elsif @state == :descent
       child = TreeNode.new(self)
-      puts "stepping one child down, from #{self.name} to #{child.name}"
+      puts "stepping one child down, from #{self.name} to #{child.name}" if @verbose
       child.consume(data)
       @child_nodes << child
       child
@@ -69,13 +69,35 @@ class TreeNode
   end
 
   def sum_up
-    puts "sum_up: children #{@child_count}, metadata_count #{@metadata_count}"
-    # @metadata.each do |part|
-    #   puts "part #{part}"
-    # end
     @metadata.reduce(0, :+) + @child_nodes.reduce(0) do |sum, child|
       sum + child.sum_up
+    end 
+  end
+
+  def sum_metadata_references
+    sum = 0
+    @metadata.each do |child_index|
+      array_index = child_index - 1 # child_index is 1 based
+      if array_index < @child_nodes.count 
+        sum += @child_nodes[array_index].value
+      else
+        # index out of bounds
+      end
     end
+    sum
+  end
+
+  def calculate_value
+    if @child_count == 0
+      # case if no chilren are present
+      sum_up
+    else
+      sum_metadata_references
+    end
+  end
+
+  def value
+    @value ||= calculate_value
   end
 end
 
@@ -85,6 +107,7 @@ class TreeBuilder
     @sequence = []
     @root_node = TreeNode.new
     @current_node = @root_node
+    @data_read = false
   end
 
   def add_line(line)
@@ -99,21 +122,35 @@ class TreeBuilder
     @sequence.each do |item|
       consume_item(item)
     end
+    @data_read = true
+  end
+
+  def read_data_if_needed
+    follow_nodes unless @data_read
   end
 
   def metadata_sum
-    follow_nodes
+    read_data_if_needed
     @root_node.sum_up
+  end
+
+  def root_value
+    read_data_if_needed
+    @root_node.value
   end
 end
 
 
+puts "example:"
 builder = TreeBuilder.new
 builder.add_line('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2')
-puts "metadata_sum: #{builder.metadata_sum}"
+puts "answer 1: #{builder.metadata_sum}"
+puts "answer 2: #{builder.root_value}"
 
+puts "test:"
 builder = TreeBuilder.new
 File.open(ARGV[0]).each_line do |line|
   builder.add_line(line.strip)
 end
 puts "answer 1: #{builder.metadata_sum}"
+puts "answer 2: #{builder.root_value}"
