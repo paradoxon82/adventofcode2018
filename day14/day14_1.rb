b8bd203b574e2a6097e434b7ae813d635592d764
Daniel Fink <daniel.ja.fink@gmail.com>
2018-12-17 09:50:32 +0100

    part 1 works

--- day14/day14_1.rb
+++ day14/day14_1.rb
@@ -25,36 +25,58 @@ class RecipeCreator
     list = (score1 + score2).to_s.split('').map(&:to_i)
     @recipies[size...(list.size + size)] = list
     @size += list.size
-    puts "new size  #{@size}"
+    #puts "new size  #{@size}"
   end
 
   def next_step
     new_recipies
-    puts "old pos #{@pos1}, #{@pos2}, size #{size}"
+    #puts "old pos #{@pos1}, #{@pos2}, size #{size}"
     @pos1 = (@pos1 + score1 + 1) % size
     @pos2 = (@pos2 + score2 + 1) % size
-    puts "new pos #{@pos1}, #{@pos2}"
+    #puts "new pos #{@pos1}, #{@pos2}"
   end
 
   def print_state
-    out = Array.new(size) do |i| 
+    print_part(@recipies, size)
+  end
+
+  def print_part(state, state_size = -1)
+    if state_size == -1
+      state_size = state.size
+    end
+    out = Array.new(state_size) do |i| 
       if i == pos1
         "(#{score1})"
       elsif i == pos2
         "[#{score2}]"
       else
-        @recipies[i].to_s
+        state[i].to_s
       end
     end
     puts out.join(' ')
   end
+
+  def print_last(part_size)
+    part = @recipies[(size-part_size)...size]
+    print_part(part)
+  end
+
+  def step_until(end_size)
+    while size < end_size
+      next_step
+    end
+  end
+ 
 end
 
 
 iterations = 47801
 
-creator = RecipeCreator.new(iterations)
+creator = RecipeCreator.new(iterations + 10)
 20.times do |iter|
   creator.print_state
   creator.next_step
 end
+puts "begin stepping.."
+creator.step_until(iterations + 10)
+creator.print_last(10)
